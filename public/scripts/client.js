017f54a949c61c71f2eeb6ba0ed41b37a799ddc7
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-19 23:14:37 +0000

    add mouseover event for icons and tweet box

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
   $(".tweet").on('mouseover', function() {
     $(".tweet").addClass("shadowBox");
   });
+
+  $(".tweet").on('mouseout', function() {
+    $(".tweet").removeClass("shadowBox");
+  });
+
+  $(".tweet i").on('mouseover', function(event) {
+    event.target.style.color = "orange";
+  });
+  
+  $(".tweet i").on('mouseout', function(event) {
+    event.target.style.color = "";
+  });
 });
