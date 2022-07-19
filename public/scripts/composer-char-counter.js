17ddc33e14d6c00f11cffebb19f36713c1f8030b
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-19 21:06:33 +0000

    working function for char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+  
+  $("#tweet-text").on('keydown', function() {
+    let textLength = $(this).val().length;
+    let charsLeft = 140 - textLength;
+    // console.log("textLength: ", textLength)
+    // console.log("charsLeft: ", charsLeft)
+    // console.log("this.parentElement.counter: ", this.parentElement.counter)
+    let counterVal = this.parentElement.counter;
+    // console.log("counterVal: ", $(counterVal));
+    $(counterVal).text(charsLeft);
+    // console.log("counterVal.val(): ", $(counterVal).val());
 
+  });
 });
