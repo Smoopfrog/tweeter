c28f9397984c2b40dee57ebb9730f9da348d2f39
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-20 22:50:13 +0000

    create load tweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,3 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const createTweetElement = testTweet => {
   const date = new Date(testTweet.created_at);
 
@@ -60,24 +35,37 @@ const renderTweets = tweets => {
   });
 }
 
+const loadTweet = () => {
+  $('#tweetFeed').empty();
+
+  $.ajax({
+    method: 'GET',
+    url: '/tweets',
+    success: (responseJSON) => {
+      renderTweets(responseJSON);
+    }
+  });
+};
+
 $(() => {
+  loadTweet();
 
   $('.tweet-form').on('submit', function(event) {
     event.preventDefault();
 
-    // get values
+    // serialize values
     let values = $(this).serialize();
 
     // posts values to /tweets
     $.ajax({
       method: 'POST',
       url:'/tweets',
-      data: values
+      data: values,
+      success: () => {
+        loadTweet();
+      }
     })
- 
   });
-
-  renderTweets(data);
 
   // Box shadow on tweet
   $(".tweet").on('mouseover', function() {
