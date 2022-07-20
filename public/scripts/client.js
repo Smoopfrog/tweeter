afaee868305a75b56cb1742637d099775eef664a
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-20 20:17:03 +0000

    add testTweet and create createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
- 
+const testTweet = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+};
+
+const createTweetElement = testTweet => {
+  console.log("Original Tweet obj:", testTweet);
+
+};
+
+
+$(() => {
+  // Box shadow on tweet
   $(".tweet").on('mouseover', function() {
     $(".tweet").addClass("shadowBox");
   });
@@ -8,6 +26,7 @@ $(document).ready(function() {
     $(".tweet").removeClass("shadowBox");
   });
 
+  // Icon color change 
   $(".tweet i").on('mouseover', function(event) {
     event.target.style.color = "orange";
   });
@@ -16,3 +35,4 @@ $(document).ready(function() {
     event.target.style.color = "";
   });
 });
+
