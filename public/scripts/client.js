a919b01c6e7453337b8d65eab2858582de24cae9
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-20 20:53:35 +0000

    add renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,30 @@
-const testTweet = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const testTweets = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 const createTweetElement = testTweet => {
-  console.log("Original Tweet obj:", testTweet);
+  const date = new Date(testTweet.created_at);
 
   const tweetTemplate = `
   <article class="tweet">
@@ -24,7 +37,7 @@ const createTweetElement = testTweet => {
     </header>
     <p>"${testTweet.content.text}"</p>
     <footer>
-      <div>"${testTweet.created_at}"</div>
+      <div>"${date}"</div>
       <div class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-heart"></i>
@@ -35,14 +48,21 @@ const createTweetElement = testTweet => {
   `;
 
   $("#tweetFeed").append(tweetTemplate);
+  };
 
-};
-
+const renderTweets = tweets => {
+  // loops through tweets
+  tweets.forEach(tweet => {  
+  // calls createTweetElement for each tweet
+    createTweetElement(tweet);  
+  });
+  
+  // takes return value and appends it to the tweets container
+}
 
 $(() => {
   
-  createTweetElement(testTweet);
-
+  renderTweets(testTweets);
   // Box shadow on tweet
   $(".tweet").on('mouseover', function() {
     $(".tweet").addClass("shadowBox");
