4239cf4fec4c521ba3ec8622fe38192350b91e94
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-20 20:24:18 +0000

    create and append the tweetTemplate to the the tweetFeed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,10 +13,36 @@ const testTweet = {
 const createTweetElement = testTweet => {
   console.log("Original Tweet obj:", testTweet);
 
+  const tweetTemplate = `
+  <article class="tweet">
+    <header>
+      <div class="iconName">
+        <img src="https://i.imgur.com/73hZDYK.png">
+      <div>Name</div>
+      </div>
+      <div id="tweeterHandle"> @tweeterhandle</div>
+    </header>
+    <p>Example Tweeter Text</p>
+    <footer>
+      <div>post date</div>
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-heart"></i>
+        <i class="fa-solid fa-retweet"></i>            
+      </div>
+    </footer>
+  </article>
+  `;
+
+  $("#tweetFeed").append(tweetTemplate);
+
 };
 
 
 $(() => {
+  
+  createTweetElement(testTweet);
+
   // Box shadow on tweet
   $(".tweet").on('mouseover', function() {
     $(".tweet").addClass("shadowBox");
@@ -34,5 +60,7 @@ $(() => {
   $(".tweet i").on('mouseout', function(event) {
     event.target.style.color = "";
   });
+
+  
 });
 
